29c0043b235cb0e0869e112693a20630b9631892
jmpredde <jaiden.preddie26@trinityschoolnyc.org>
2025-06-01 15:39:26 -0400

    added svg and html pages

--- server.py
+++ server.py
@@ -8,10 +8,21 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    f = open('data/data.json','r')
+    complete_data = json.load(f)
+    f.close()
+    Jan_dates = complete_data.keys()
+    return render_template('index.html',dates= Jan_dates)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/date')
+def date():
+    f = open('data/data.json','r')
+    complete_data = json.load(f)
+    f.close()
+    return render_template('date.html')
 
 app.run(debug=True)
